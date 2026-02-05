b0c094dabbfb04c2d185beaae7c9fd07f7c23208
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-02-05 12:43:08 -0300

    REFACTOR: Updating the Imports in ./main.py

--- main.py
+++ main.py
@@ -51,7 +51,10 @@ import atexit  # For playing a sound when the program finishes
 import datetime  # For getting the current date and time
 import os  # For running a command in the terminal
 import platform  # For getting the operating system name
+import re  # For regular expressions
+import requests  # For making HTTP requests
 import sys  # For system-specific parameters and functions
+from bs4 import BeautifulSoup  # For HTML parsing
 from colorama import Style  # For coloring the terminal
 from Logger import Logger  # For logging output to both terminal and file
 from pathlib import Path  # For handling file paths
